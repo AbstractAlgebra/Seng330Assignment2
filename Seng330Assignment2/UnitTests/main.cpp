aa292898c01a974cfeed268449b46955debe82ca
AbstractAlgebran <fatlus@mail.com>
2015-11-14 16:03:52 -0800

    added gtest framework

---
+++ Seng330Assignment2/UnitTests/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UnitTests
+//
+//  Created by Jagger Cox on 2015-11-14.
+//  Copyright © 2015 seng330. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
