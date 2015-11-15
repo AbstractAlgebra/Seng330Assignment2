1885ba378ab2b22299eb5913b526ac2af41b0651
AbstractAlgebran <fatlus@mail.com>
2015-11-14 17:31:16 -0800

    Added my first google test case

---
+++ Seng330Assignment2/Seng330Assignment2/add.cpp
@@ -0,0 +1,9 @@
+//
+//  add.cpp
+//  Seng330Assignment2
+//
+//  Created by Jagger Cox on 2015-11-14.
+//  Copyright © 2015 seng330. All rights reserved.
+//
+
+#include "add.hpp"
---
+++ Seng330Assignment2/Seng330Assignment2/add.hpp
@@ -0,0 +1,14 @@
+//
+//  add.hpp
+//  Seng330Assignment2
+//
+//  Created by Jagger Cox on 2015-11-14.
+//  Copyright © 2015 seng330. All rights reserved.
+//
+
+#ifndef add_hpp
+#define add_hpp
+
+#include <stdio.h>
+
+#endif /* add_hpp */
